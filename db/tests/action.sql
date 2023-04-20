3eff2b734e3defcd490a7a5f4c8ec5c7cd76985e
Jarrett Keifer <jkeifer@users.noreply.github.com>
2023-04-20 14:51:01 -0700

    Initial schema and testing (#3)
    * sql linting/formatting with precommit
    
    * dbmate testing with docker
    
    * update pg docker to have pg_partman extension
    
    * move pg Dockerfile and add pgTap for testing
    
    * pg docker updates
    
    - add pg_prove
    - add dbmate
    - refactor Dockerfile
    - mount project in pg container for tools
    - turn off pg password requirement
    
    * base schema in place with (minimal) tests
    
    * locking fixes
    
    * split thread insert and update into two triggers
    
    * add database readme
    
    * Updating readme ref
    
    * changes based on PR feedback
    
    * dbmate-tupac: a dbmate wrapper for better schemas (#5)
    
    * dbmate-tupac: a dbmate wrapper for better schemas
    
    I have a problem with dbmate's schema handling. It will dump the entire
    database "schema only" into a file, and expects that file to be useful
    for reference. But much of the file can end up being changes due to
    things outside the current project's concerns.
    
    For example, enabling an extension that creates functions/tables in the
    database will cause those to show up in the schema file. And things like
    partitions that change over time: even if nothing changes in the
    migrations, the dumped schema can show changes.
    
    The "schema only" dump also excludes data that is effectively part of
    the schema itself. For example, the `thread_state` table defines a set
    of states that can be used for the `thread.status` field, and those rows
    are a fixed set of values enforcing the set of possible `thread.status`
    values. In effect, these "data" rows are as important as the info in a
    "schema only" dump to ensuring the database has the information it needs
    to work as expected.
    
    As a result, it seems that the schema handling of dbmate does not give
    us what we want, so I created this wrapper implementing a new `verify`
    command that I think makes more sense. It does the following:
    
      - creates the database from the reference schema
      - dumps the database
      - drops the database
      - creates the database and applies all migrations
      - dumps the database
      - drops the database
      - diffs the dumps
    
    If the diff shows no changes then the migrations and the reference
    schema are aligned.
    
    Functionally, this means that dbmate commands no longer dump the schema
    into `schema.sql`, and we end up maintaining that file ourselves as the
    true schema the migraitons must match.
    
    This means that some changes will need to be made both in a migration
    and in the reference schema. This is additional work. But it gives us a
    way to actually enforce that the schema is an effective reference for
    what we want to have in the database. In other words, this gives us a
    reference schema that is not subject to the limitations of the "schema
    only" dump supported by dbmate.
    
    * add test command to dbmate-tupac
    
    * fixup pg dockerfile so dbmate is dbmate-tupac
    
    * update database readme
    
    * update database readme with additional context
    
    ---------
    
    Co-authored-by: Matt Bialas <mbialas@element84.com>

---
+++ db/tests/action.sql
@@ -0,0 +1,240 @@
+BEGIN;
+
+SET search_path = tap, public;
+SELECT plan(13);
+
+INSERT INTO swoop.action (
+  action_uuid,
+  action_type,
+  handler_name,
+  action_name,
+  created_at
+) VALUES (
+  'b15120b8-b7ab-4180-9b7a-b0384758f468'::uuid,
+  'workflow',
+  'argo-workflow',
+  'workflow-a',
+  '2023-04-13 00:25:07.388012+00'::timestamptz
+);
+
+-- check event created as expected
+SELECT results_eq(
+  $$
+    SELECT
+      event_time,
+      action_uuid,
+      status,
+      retry_seconds,
+      error
+    FROM
+      swoop.event
+    WHERE action_uuid = 'b15120b8-b7ab-4180-9b7a-b0384758f468'
+  $$,
+  $$
+    VALUES (
+      '2023-04-13 00:25:07.388012+00'::timestamptz,
+      'b15120b8-b7ab-4180-9b7a-b0384758f468'::uuid,
+      'PENDING',
+      null::integer,
+      null
+    )
+  $$,
+  'event should be created on action insert'
+);
+
+-- check thread created as expected
+SELECT results_eq(
+  $$
+    SELECT
+      last_update,
+      action_uuid,
+      status,
+      next_attempt_after
+    FROM
+      swoop.thread
+    WHERE
+      action_uuid = 'b15120b8-b7ab-4180-9b7a-b0384758f468'
+  $$,
+  $$
+    VALUES (
+      '2023-04-13 00:25:07.388012+00'::timestamptz,
+      'b15120b8-b7ab-4180-9b7a-b0384758f468'::uuid,
+      'PENDING',
+      null::timestamptz
+    )
+  $$,
+  'thread should be created on event insert'
+);
+
+-- get the processable action
+SELECT is_empty(
+  $$
+    SELECT swoop.get_processable_actions(
+      _ignored_action_uuids => array[]::uuid[],
+      _handler_names => array['bogus']
+    )
+  $$,
+  'should not return any processable actions - bad action name'
+);
+
+SELECT is_empty(
+  $$
+    SELECT swoop.get_processable_actions(
+      _ignored_action_uuids => array['b15120b8-b7ab-4180-9b7a-b0384758f468'::uuid]
+    )
+  $$,
+  'should not return any processable actions - filtered action uuid'
+);
+
+SELECT results_eq(
+  $$
+    SELECT
+      action_uuid,
+      handler_name
+    FROM
+      swoop.get_processable_actions(
+        _ignored_action_uuids => array[]::uuid[],
+        _handler_names => array['argo-workflow']
+      )
+  $$,
+  $$
+    SELECT
+      action_uuid,
+      handler_name
+    FROM
+      swoop.action
+    WHERE
+      action_uuid = 'b15120b8-b7ab-4180-9b7a-b0384758f468'
+  $$,
+  'should get our processable action'
+);
+
+-- check locks
+SELECT results_eq(
+  $$
+    SELECT
+      classid,
+      objid
+    FROM
+      pg_locks
+    WHERE
+      locktype = 'advisory'
+  $$,
+  $$
+    SELECT
+      to_regclass('swoop.thread')::oid,
+      lock_id::oid
+    FROM
+      swoop.thread
+    WHERE
+      action_uuid = 'b15120b8-b7ab-4180-9b7a-b0384758f468'
+  $$,
+  'should have an advisory lock for the processable action we grabbed'
+);
+
+-- insert backoff event for action, drop lock,
+-- check thread update, and check processable
+INSERT INTO swoop.event (
+  event_time,
+  action_uuid,
+  status,
+  retry_seconds,
+  error
+) VALUES (
+  '2023-04-13 00:25:08.388012+00'::timestamptz,
+  'b15120b8-b7ab-4180-9b7a-b0384758f468'::uuid,
+  'BACKOFF',
+  1,
+  'some error string'
+);
+
+SELECT
+  ok(
+    swoop.unlock_thread(lock_id),
+    'should release the lock on our row'
+  )
+FROM swoop.thread
+WHERE
+  action_uuid = 'b15120b8-b7ab-4180-9b7a-b0384758f468'::uuid;
+
+SELECT is_empty(
+  $$
+    SELECT
+      classid,
+      objid
+    FROM
+      pg_locks
+    WHERE
+      locktype = 'advisory'
+  $$,
+  'should not have any advisory locks'
+);
+
+SELECT
+  matches(
+    status,
+    'BACKOFF',
+    'thread status should be backoff'
+  ) AS matches
+FROM swoop.thread;
+
+SELECT
+  cmp_ok(
+    next_attempt_after,
+    '=',
+    last_update + interval '1 second',
+    'thread next attempt should be last update plus backoff time'
+  ) AS cmp
+FROM swoop.thread;
+
+SELECT results_eq(
+  $$
+    SELECT
+      action_uuid,
+      handler_name
+    FROM
+      swoop.get_processable_actions(
+        _ignored_action_uuids => array[]::uuid[],
+        _handler_names => array['argo-workflow']
+      )
+  $$,
+  $$
+    SELECT action_uuid, handler_name
+    FROM swoop.action
+  $$,
+  'should get our processable action in the backoff state'
+);
+
+-- insert queued event, drop lock, and check it is not processable
+INSERT INTO swoop.event (
+  event_time,
+  action_uuid,
+  status
+) VALUES (
+  '2023-04-13 00:25:10.388012+00'::timestamptz,
+  'b15120b8-b7ab-4180-9b7a-b0384758f468'::uuid,
+  'QUEUED'
+);
+
+SELECT
+  ok(
+    swoop.unlock_thread(lock_id),
+    'should release the lock on our row once more'
+  )
+FROM swoop.thread
+WHERE
+  action_uuid = 'b15120b8-b7ab-4180-9b7a-b0384758f468'::uuid;
+
+
+SELECT is_empty(
+  $$
+    SELECT swoop.get_processable_actions(
+      _ignored_action_uuids => array[]::uuid[]
+    )
+  $$,
+  'should not return any processable actions due to state'
+);
+
+
+SELECT * FROM finish(); -- noqa
+ROLLBACK;
---
+++ db/tests/limit-locking.sql
@@ -0,0 +1,63 @@
+BEGIN;
+
+SET search_path = tap, public;
+SELECT plan(3);
+
+DO
+$$
+BEGIN
+  FOR i in 1..300 LOOP
+    INSERT INTO swoop.action (
+      action_uuid,
+      action_type,
+      handler_name,
+      action_name,
+      created_at
+    ) VALUES (
+      gen_random_uuid(),
+      'workflow',
+      'argo-workflow',
+      'workflow-a',
+      now()
+    );
+  END LOOP;
+END;
+$$;
+
+SELECT
+  is(
+    count(*),
+    300::bigint,
+    'should have expected number of processable threads'
+  )
+FROM
+  swoop.thread AS t --noqa: AL05
+WHERE
+  swoop.thread_is_processable(t);
+
+SELECT
+  is(
+    count(*),
+    10::bigint,
+    'should get expected number of processable actions'
+  )
+FROM
+  swoop.get_processable_actions(
+    _ignored_action_uuids => ARRAY[]::uuid []
+  );
+
+SELECT
+  is(
+    count(*),
+    10::bigint,
+    'should have exepcted number of locks on threads'
+  )
+FROM
+  pg_locks
+WHERE
+  locktype = 'advisory'
+  AND classid = to_regclass('swoop.thread')::oid;
+
+
+SELECT * FROM finish(); -- noqa
+ROLLBACK;
