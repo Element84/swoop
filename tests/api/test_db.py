47a937aef832b415e5f055dd4388b083ed861dd0
Matt Bialas <mbialas@element84.com>
2023-04-21 14:53:02 -0400

    Fixing connection warning

--- tests/api/test_db.py
+++ tests/api/test_db.py
@@ -8,14 +8,19 @@ async def test_db_connection_pool():
 
     pools = await connect_to_db(settings)
     readpool = pools[0]
+    connections = []
 
     # Saturate the connection pool
     for i in range(settings.db_max_conn_size):
-        await readpool.acquire()
+        connections.append(await readpool.acquire())
 
     # Expecting a Timeout (after 1s), with no connections available
     with pytest.raises(TimeoutError):
-        await readpool.acquire(timeout=1)
+        connections.append(await readpool.acquire(timeout=1))
 
-    readpool.close()
+    # Release all connections
+    for c in connections:
+        await readpool.release(c, timeout=1)
+
+    await readpool.close()
     assert True
