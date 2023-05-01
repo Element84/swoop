b9aafaf57ccb19717588b6f16d13f1d46f98d0c3
Hector Machin <hector.machin@ursaspace.com>
2023-05-01 11:18:46 -0400

    Adding processes endpoint

---
+++ tests/api/test_processes.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+import pytest
+import json
+
+
+@pytest.mark.asyncio
+async def test_get_all_processes(test_app):
+    with TestClient(test_app) as app_client:
+
+        response = app_client.get('/processes/test')
+        assert response.status_code == 200
+        assert '2595f2da-81a6-423c-84db-935e6791046e' in json.dumps(response.json())
+
+
