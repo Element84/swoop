8e12a4fe69a1cde2b2b593c699a8bb57bfd729f3
Hector Machin <38859646+hectormachin@users.noreply.github.com>
2023-05-10 16:34:12 -0400

    Update src/swoop/api/workflows.py
    Co-authored-by: Jarrett Keifer <jkeifer@users.noreply.github.com>

--- src/swoop/api/workflows.py
+++ src/swoop/api/workflows.py
@@ -4,4 +4,4 @@ import yaml
 
 def init_workflows_config(app: FastAPI) -> None:
     """Initialize Workflow Config."""
-    app.state.workflows = yaml.full_load(open(app.state.settings.workflow_config_file))
+    app.state.workflows = yaml.safe_load(open(app.state.settings.workflow_config_file))
