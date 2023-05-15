4579599077bd194adfadfb63d11dc08f272e5785
Hector Machin <hector.machin@ursaspace.com>
2023-05-15 10:26:41 -0400

    Additional tests for codecoverage

--- tests/api/test_config.py
+++ tests/api/test_config.py
@@ -1,10 +1,25 @@
+import pytest
+
 from swoop.api.config import Settings
 
 
-def test_loads_from_env_file():
+def test_loads_from_env_vars():
     """
-    Simple test to verify we can load settings from an
-    env file.
+    Simple test to verify we can load settings from a
+    env variables.
     """
     settings = Settings(".env")
     assert settings.db_name == "swoop"
+
+
+def test_no_loads_from_env_file():
+    """
+    Simple test to verify we cannot load settings from an
+    env file.
+    """
+    with pytest.raises(ValueError) as swoop_settings:
+        Settings(_env_file=".env")
+    assert (
+        str(swoop_settings.value)
+        == "swoop settings does not support loading an env file"
+    )
--- tests/api/test_db.py
+++ tests/api/test_db.py
@@ -30,3 +30,29 @@ async def test_db_connection_pool() -> None:
 
     await close_db_connection(app)
     assert True
+
+
+@pytest.mark.asyncio
+async def test_db_connection_pool_with_reader_host() -> None:
+    app: FastAPI = get_app()
+
+    # Testing connection pool with db writer host
+    app.state.settings.db_writer_host = app.state.settings.db_reader_host
+    await connect_to_db(app)
+
+    connections = []
+
+    # Saturate the connection pool
+    for _ in range(app.state.settings.db_max_conn_size):
+        connections.append(await app.state.readpool.acquire())
+
+    # Expecting a Timeout (after 1s), with no connections available
+    with pytest.raises(TimeoutError):
+        connections.append(await app.state.readpool.acquire(timeout=0.1))
+
+    # Release all connections
+    for c in connections:
+        await app.state.readpool.release(c, timeout=1)
+
+    await close_db_connection(app)
+    assert True
