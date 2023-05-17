0783a5b81a4feb19fb75bf10b41393cf9d3ddbfe
Hector Machin <hector.machin@ursaspace.com>
2023-05-17 17:17:26 -0400

    Adding write object methods for IOClient

--- src/swoop/api/io.py
+++ src/swoop/api/io.py
@@ -1,3 +1,4 @@
+import io
 import logging
 
 from minio import Minio
@@ -7,7 +8,6 @@ logger = logging.getLogger("uvicorn")
 logger.setLevel(logging.INFO)
 
 
-# Change this file to an IO class
 class IOClient:
     client = None
     bucket_name = None
@@ -39,6 +39,7 @@ class IOClient:
                 try:
                     response = self.client.get_object(self.bucket_name, object_name)
                     object_response = response.json()
+                    logger.debug(f"retrieved object content: {object_response}")
                 except S3Error as err:
                     logger.debug(err)
                 finally:
@@ -48,3 +49,40 @@ class IOClient:
                 logger.debug(err)
 
         return object_response
+
+    def put_file_object(self, object_name: str, file_name: str):
+        if (
+            object_name
+            and file_name
+            and self.bucket_name
+            and self.client.bucket_exists(self.bucket_name)
+        ):
+            result = self.client.fput_object(self.bucket_name, object_name, file_name)
+            logger.debug(
+                "created {} object; etag: {}, version-id: {}".format(
+                    result.object_name, result.etag, result.version_id
+                )
+            )
+
+    def put_object(
+        self, object_name: str, object_content: str, content_type="application/json"
+    ):
+        if (
+            object_name
+            and object_content
+            and content_type
+            and self.bucket_name
+            and self.client.bucket_exists(self.bucket_name)
+        ):
+            result = self.client.put_object(
+                self.bucket_name,
+                object_name,
+                io.BytesIO(object_content.encode("utf-8")),
+                len(object_content.encode("utf-8")),
+                content_type,
+            )
+            logger.debug(
+                "created {} object; etag: {}, version-id: {}".format(
+                    result.object_name, result.etag, result.version_id
+                )
+            )
