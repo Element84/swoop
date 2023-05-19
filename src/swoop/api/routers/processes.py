0a3a96c280585b151dcae2d003b97834669f8a86
jkeifer <jkeifer@element84.com>
2023-05-19 13:24:28 -0700

    clean up process execute insert queries

--- src/swoop/api/routers/processes.py
+++ src/swoop/api/routers/processes.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import json
 import uuid
 
-from buildpg import render
+from buildpg import Values, render
 from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from pydantic import BaseModel
 
@@ -102,12 +102,7 @@ async def get_process_description(
     except KeyError:
         raise HTTPException(status_code=404, detail="Process not found")
 
-@router.get("/{process_id}/definition")
-async def get_process_definition(process_id: str = Path(..., alias="processID")) -> str:
-    """
-    retrieve a process definition
-    """
-    pass
+    return to_process_summary(workflow)
 
 
 @router.post(
@@ -144,38 +139,32 @@ async def execute_process(
     )
 
     async with request.app.state.readpool.acquire() as conn:
-        # Insert a dummy row into payload_cache table
-        # To be removed later
-        pl_uuid = "debeb36c-e09e-41c3-bdc5-596287fe724a"
+        async with conn.transaction():
+            # Insert a dummy row into payload_cache table
+            # To be removed later
+            pl_uuid = "debeb36c-e09e-41c3-bdc5-596287fe724a"
 
-        q, p = render(
-            """
-                INSERT INTO swoop.payload_cache(payload_uuid,
-                    workflow_version, workflow_name)
-                VALUES (:payload_uuid::uuid,:workflow_version::smallint,
-                :workflow_name::text);
-            """,
-            payload_uuid=pl_uuid,
-            workflow_version=2,
-            workflow_name="mirror",
-        )
+            q, p = render(
+                "INSERT INTO swoop.payload_cache (:values__names) VALUES :values",
+                values=Values(
+                    payload_uuid=pl_uuid,
+                    workflow_version=2,
+                    workflow_name="mirror",
+                ),
+            )
 
-        await conn.fetchrow(q, *p)
+            await conn.execute(q, *p)
 
-        q, p = render(
-            """
-                INSERT INTO swoop.action(action_uuid, action_type,
-                    action_name, handler_name, parent_uuid, payload_uuid)
-                VALUES (:action_uuid::uuid,:action_type::text,:action_name::text,
-                :handler_name::text,:parent_uuid::uuid,:payload_uuid::uuid);
-            """,
-            action_uuid=action_uuid,
-            action_type="workflow",
-            action_name=workflow.__root__.name,
-            handler_name=workflow.__root__.handler,
-            parent_uuid=uuid.uuid4(),
-            payload_uuid=pl_uuid,
-        )
-        await conn.fetchrow(q, *p)
+            q, p = render(
+                "INSERT INTO swoop.action (:values__names) VALUES :values",
+                values=Values(
+                    action_uuid=action_uuid,
+                    action_type="workflow",
+                    action_name=workflow.name,
+                    handler_name=workflow.handler,
+                    payload_uuid=pl_uuid,
+                ),
+            )
+            await conn.execute(q, *p)
 
     return await get_job_status(request, job_id=action_uuid)
