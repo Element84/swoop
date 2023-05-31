1c3b4cedbe445db86f03a1604ea96bf4d0654170
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-05-31 09:44:08 -0500

    update for ruff to pass

--- tests/api/test_payloads.py
+++ tests/api/test_payloads.py
@@ -4,6 +4,7 @@ from ..conftest import inject_database_fixture
 
 inject_database_fixture(["base_01"], __name__)
 
+
 @pytest.fixture
 def all_payloads():
     return {
@@ -23,9 +24,11 @@ def all_payloads():
         ],
     }
 
+
 @pytest.fixture
 def no_payloads_exception():
-    return {'detail': 'No payloads that match query parameters found'}
+    return {"detail": "No payloads that match query parameters found"}
+
 
 @pytest.fixture
 def no_payload_id_exception():
@@ -34,11 +37,13 @@ def no_payload_id_exception():
 
 # Tests for GET/payloads endpoint
 
+
 @pytest.mark.asyncio
 async def test_get_payloads_no_filter(test_client, all_payloads):
     response = test_client.get("/payloads")
     assert response.json() == all_payloads
     assert response.status_code == 200
+
 
 @pytest.mark.asyncio
 async def test_get_payloads_filter_limit_only(test_client, all_payloads):
@@ -46,62 +51,97 @@ async def test_get_payloads_filter_limit_only(test_client, all_payloads):
     assert response.json() == all_payloads
     assert response.status_code == 200
 
+
 @pytest.mark.asyncio
 async def test_get_payloads_filter_limit_process_only(test_client):
     response = test_client.get("/payloads/?limit=1000&process_id=some_workflow")
     assert response.status_code == 200
 
-@pytest.mark.asyncio
-async def test_get_payloads_filter_limit_process_collection_only(test_client):
-    response = test_client.get("/payloads/?limit=1000&process_id=some_workflow&collection_id=collection1")
-    assert response.status_code == 200
 
 @pytest.mark.asyncio
-async def test_get_payloads_filter_invalid_collection_id(test_client, no_payloads_exception):
-    response = test_client.get("/payloads/?limit=1000&process_id=some_workflow&collection_id=collection5")
+async def test_get_payloads_filter_limit_process_collection_only(test_client):
+    response = test_client.get(
+        "/payloads/?limit=1000&process_id=some_workflow&collection_id=collection1"
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_get_payloads_filter_invalid_collection_id(
+    test_client, no_payloads_exception
+):
+    response = test_client.get(
+        "/payloads/?limit=1000&process_id=some_workflow&collection_id=collection5"
+    )
     assert response.json() == no_payloads_exception
     assert response.status_code == 404
+
 
 @pytest.mark.asyncio
 async def test_get_payloads_filter_invalid_item_id(test_client, no_payloads_exception):
-    response = test_client.get("/payloads/?limit=1000&process_id=some_workflow&collection_id=collection1&item_id=id5")
+    response = test_client.get(
+        "/payloads/?limit=1000&process_id=some_workflow&\
+            collection_id=collection1&item_id=id5"
+    )
     assert response.json() == no_payloads_exception
     assert response.status_code == 404
 
+
 @pytest.mark.asyncio
 async def test_get_payloads_filter_all(test_client):
-    response = test_client.get("/payloads/?limit=1000&process_id=some_workflow&collection_id=collection1&item_id=id1")
+    response = test_client.get(
+        "/payloads/?limit=1000&process_id=some_workflow&\
+            collection_id=collection1&item_id=id1"
+    )
     assert response.status_code == 200
+
 
 @pytest.mark.asyncio
 async def test_get_payloads_filter_collection_multiple_items(test_client):
-    response = test_client.get("/payloads/?limit=1000&process_id=some_workflow&collection_id=collection1&item_id=id1&item_id=id2&item_id=id3")
+    response = test_client.get(
+        "/payloads/?limit=1000&process_id=some_workflow&\
+            collection_id=collection1&item_id=id1&item_id=id2&item_id=id3"
+    )
     assert response.status_code == 200
+
 
 @pytest.mark.asyncio
 async def test_get_payloads_filter_multiple_collection(test_client):
-    response = test_client.get("/payloads/?limit=1000&process_id=some_workflow&collection_id=collection1&collection_id=collection2")
+    response = test_client.get(
+        "/payloads/?limit=1000&process_id=some_workflow&\
+            collection_id=collection1&collection_id=collection2"
+    )
     assert response.status_code == 200
+
 
 @pytest.mark.asyncio
 async def test_get_payloads_filter_only_multiple_items(test_client):
     response = test_client.get("/payloads/?limit=1000&item_id=id1&item_id=id2")
     assert response.status_code == 200
 
+
 @pytest.mark.asyncio
-async def test_get_payloads_filter_only_invalid_item_id(test_client, no_payloads_exception):
+async def test_get_payloads_filter_only_invalid_item_id(
+    test_client, no_payloads_exception
+):
     response = test_client.get("/payloads/?limit=1000&item_id=id8")
     assert response.json() == no_payloads_exception
     assert response.status_code == 404
 
+
 @pytest.mark.asyncio
-async def test_get_payloads_filter_only_invalid_collection_id(test_client, no_payloads_exception):
+async def test_get_payloads_filter_only_invalid_collection_id(
+    test_client, no_payloads_exception
+):
     response = test_client.get("/payloads/?limit=1000&collection_id=collection3")
     assert response.json() == no_payloads_exception
     assert response.status_code == 404
 
+
 @pytest.mark.asyncio
-async def test_get_payloads_filter_only_invalid_process_id(test_client, no_payloads_exception):
+async def test_get_payloads_filter_only_invalid_process_id(
+    test_client, no_payloads_exception
+):
     response = test_client.get("/payloads/?limit=1000&process_id=hello")
     assert response.json() == no_payloads_exception
     assert response.status_code == 404
@@ -109,10 +149,12 @@ async def test_get_payloads_filter_only_invalid_process_id(test_client, no_paylo
 
 # Tests for GET/payloads/{payload-id} endpoint
 
+
 @pytest.mark.asyncio
 async def test_get_payloadid_match(test_client):
     response = test_client.get("/payloads/ade69fe7-1d7d-472e-9f36-7242cc2aca77")
     assert response.status_code == 200
+
 
 @pytest.mark.asyncio
 async def test_get_payloadid_no_match(test_client, no_payload_id_exception):
