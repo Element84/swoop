24f16e174e2912f5821a336a9acc825cdd8528b3
Hector Machin <hector.machin@ursaspace.com>
2023-06-22 12:54:17 -0400

    Fixing environment variable reference

--- tests/testkube/k6/swoop-api-simple-k6-load-test.js
+++ tests/testkube/k6/swoop-api-simple-k6-load-test.js
@@ -11,15 +11,15 @@ export const options = {
 };
 
 export default function() {
-  const swoopApiRoot = http.get('http://{{.apiHost}}/');
+  const swoopApiRoot = http.get('http://' + __ENV.API_HOST + '/');
   check(swoopApiRoot, {
     'status of SWOOP API root test was 200': (r) => r.status == 200
   });
-  const swoopApiJobs = http.get('http://{{.apiHost}}/jobs/');
+  const swoopApiJobs = http.get('http://' + __ENV.API_HOST + '/jobs/');
   check(swoopApiJobs, {
     'status of SWOOP API jobs endpoint (with swoop-db) test was 200': (r) => r.status == 200
   });
-  const swoopApiJobPayload = http.get('http://{{.apiHost}}/jobs/2595f2da-81a6-423c-84db-935e6791046e/payload');
+  const swoopApiJobPayload = http.get('http://' + __ENV.API_HOST + '/jobs/2595f2da-81a6-423c-84db-935e6791046e/payload');
   check(swoopApiJobPayload, {
     'status of SWOOP API payload input (with object storage) test was 200': (r) => r.status == 200
   });
