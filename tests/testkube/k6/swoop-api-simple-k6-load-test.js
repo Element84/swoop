f6f48493dd612229e28ace429f586e5dd642d768
Hector Machin <hector.machin@ursaspace.com>
2023-06-22 12:46:37 -0400

    Adding k6 load test

---
+++ tests/testkube/k6/swoop-api-simple-k6-load-test.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+import {check,sleep} from 'k6';
+
+export const options = {
+  stages: [
+    {
+      duration: '5s',
+      target: 10
+    },
+  ],
+};
+
+export default function() {
+  const swoopApiRoot = http.get('http://{{.apiHost}}/');
+  check(swoopApiRoot, {
+    'status of SWOOP API root test was 200': (r) => r.status == 200
+  });
+  const swoopApiJobs = http.get('http://{{.apiHost}}/jobs/');
+  check(swoopApiJobs, {
+    'status of SWOOP API jobs endpoint (with swoop-db) test was 200': (r) => r.status == 200
+  });
+  const swoopApiJobPayload = http.get('http://{{.apiHost}}/jobs/2595f2da-81a6-423c-84db-935e6791046e/payload');
+  check(swoopApiJobPayload, {
+    'status of SWOOP API payload input (with object storage) test was 200': (r) => r.status == 200
+  });
+}
