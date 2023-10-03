80a9c31b7f6c5459cbfe889e852ac7b56a02094d
jkeifer <jkeifer@element84.com>
2023-10-02 20:23:52 -0700

    ioclient fix weirdness with bucket name mutations

--- tests/conftest.py
+++ tests/conftest.py
@@ -67,12 +67,12 @@ def generate_io_fixture(fixtures, io_postfix=None, scope="module"):
                     )
 
         try:
-            ioclient.create_bucket(bucket_name)
+            ioclient.create_bucket()
             setup_io(ioclient)
             yield bucket_name
         finally:
             ioclient.delete_objects()
-            ioclient.delete_bucket(bucket_name)
+            ioclient.delete_bucket()
 
     return io_fixture
 
